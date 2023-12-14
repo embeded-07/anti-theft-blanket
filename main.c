d4397e4c993086ad3a0637b3744fa5fce3791af4
edac99 <jamjam1208@naver.com>
2023-12-14 21:56:01 +0900

    feat: 블루투스 첫 연결

--- main.c
+++ main.c
@@ -12,15 +12,27 @@
 
 /* function prototype */
 void RCC_Configure(void);
+
 void Slope_GPIO_Configure(void);
 void Light_GPIO_Configure(void);
 void Input_GPIO_Configure(void);
+void USART_GPIO_Configure(void);
+
 void EXTI_Configure(void);
 void ADC_Configure(void);
+
+void USART1_Init(void);
+void USART2_Init(void);
+
 void ADC1_2_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
+
 void Slope_NVIC_Configure(void);
 void Light_NVIC_Configure(void);
-void EXTI15_10_IRQHandler(void);
+void USART_NVIC_Configure(void);
+
 void Delay(void);
 void StatusInit(void);
 
@@ -34,6 +46,7 @@ uint16_t brightValue = 0;
 
 void RCC_Configure(void)
 {
+  // Alternate Function IO clock enable
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
   // PC10 - 기울기 센서, 버튼
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
@@ -41,6 +54,11 @@ void RCC_Configure(void)
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
   // LED, 부저
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+  // USART1, USART2 TX/RX port clock enable
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  // USART1, USART2 clock enable
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 }
 
 // 기울기 센서 초기화 - PC10
@@ -74,6 +92,7 @@ void Input_GPIO_Configure()
   GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
+// LED, 부저
 void Output_GPIO_Configure()
 {
   GPIO_InitTypeDef GPIO_InitStructure;
@@ -81,6 +100,35 @@ void Output_GPIO_Configure()
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+void USART_GPIO_Configure(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* USART1 pin setting */
+  // TX
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  // RX
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD | GPIO_Mode_IPU;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* USART2 pin setting */
+  // TX
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  // RX
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD | GPIO_Mode_IPU;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
 void EXTI_Configure(void)
@@ -128,6 +176,42 @@ void ADC_Configure(void)
   ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 }
 
+void USART1_Init(void)
+{
+  USART_InitTypeDef USART1_InitStructure;
+
+  // Enable the USART1 peripheral
+  USART_Cmd(USART1, ENABLE);
+
+  USART1_InitStructure.USART_BaudRate = 9600;
+  USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART1_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART1_InitStructure.USART_Parity = USART_Parity_No;
+  USART1_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART1, &USART1_InitStructure);
+
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
+
+void USART2_Init(void)
+{
+  USART_InitTypeDef USART2_InitStructure;
+
+  // Enable the USART2 peripheral
+  USART_Cmd(USART2, ENABLE);
+
+  USART2_InitStructure.USART_BaudRate = 9600;
+  USART2_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  USART2_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART2_InitStructure.USART_Parity = USART_Parity_No;
+  USART2_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART2_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_Init(USART2, &USART2_InitStructure);
+
+  USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+}
+
 // 기울기 센서
 void Slope_NVIC_Configure(void)
 {
@@ -156,6 +240,61 @@ void Light_NVIC_Configure()
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 
   NVIC_Init(&NVIC_InitStructure);
+}
+
+void USART_NVIC_Configure(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+
+  // USART1
+  // 'NVIC_EnableIRQ' is only required for USART setting
+  NVIC_EnableIRQ(USART1_IRQn);
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;        // TODO
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  // USART2
+  // 'NVIC_EnableIRQ' is only required for USART setting
+  NVIC_EnableIRQ(USART2_IRQn);
+  NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;        // TODO
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+void USART1_IRQHandler(void)
+{ // pc->폰
+  uint16_t word;
+  if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+  {
+    // the most recent received data by the USART1 peripheral
+    word = USART_ReceiveData(USART1);
+
+    USART_SendData(USART2, word);
+
+    // clear 'Read data register not empty' flag
+    USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+  }
+}
+
+void USART2_IRQHandler(void)
+{ //  폰->pc
+  uint16_t word;
+  if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+  {
+    // the most recent received data by the USART2 peripheral
+    word = USART_ReceiveData(USART2);
+
+     USART_SendData(USART1, word);
+
+    // clear 'Read data register not empty' flag
+    USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+  }
 }
 
 // 조도 센서 값 핸들링
@@ -195,7 +334,7 @@ void Delay(void)
   }
 }
 
-int getSlopeSensorValue()
+int getSlopeSensorValue(void)
 {
   return GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_10);
 }
@@ -218,12 +357,17 @@ int main(void)
   Light_GPIO_Configure();
   Input_GPIO_Configure();
   Output_GPIO_Configure();
+  USART_GPIO_Configure();
 
   ADC_Configure();
   EXTI_Configure();
 
+  USART1_Init();
+  USART2_Init();
+
   Slope_NVIC_Configure();
   Light_NVIC_Configure();
+  USART_NVIC_Configure();
 
   LCD_Init();
   Touch_Configuration();
@@ -235,7 +379,7 @@ int main(void)
     while (startSignal != 1)
     {
       startSignal = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_4);
-      LCD_ShowString(0, 0, "WAITING...", BLACK, WHITE);
+      LCD_ShowString(0, 0, "...", BLACK, WHITE);
       if (startSignal == 1)
       {
         StatusInit();
@@ -244,27 +388,42 @@ int main(void)
       }
     }
 
-    // START
-    LCD_ShowString(0, 0, "START!!", BLACK, WHITE);
-
     // 조도 센서 밝기 감지
     if (lightFlag == 1)
     {
       GPIO_SetBits(GPIOD, GPIO_Pin_2 | GPIO_Pin_3);
       Delay();
       GPIO_ResetBits(GPIOD, GPIO_Pin_3);
-      LCD_ShowString(0, 0, "EMERGENCY!!", RED, WHITE);
+      LCD_Clear(WHITE);
+      LCD_ShowString(0, 0, "DANGER!!!", RED, WHITE);
+
+      // Bluetooth
+      // int i = 0;
+      // char str[] = "WARNING!\r\n";
+      // while (str[i] != 0)
+      // {
+      //   LCD_Clear(WHITE);
+      //   LCD_ShowString(100, 100, str[i], RED, WHITE);
+      //   while ((USART1->SR & USART_SR_TC) == 0)
+      //     ;
+      //   USART_SendData(USART2, str[i]);
+      //   i++;
+      // }
+      USART_SendData(USART2, 'Y');
     }
     // 기울기 센서 변화 감지
     else if (slopeFlag == 1)
     {
       // 조도 센서 값 읽기
       ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-      LCD_ShowString(0, 0, "SLOPE ON", RED, WHITE);
+      LCD_Clear(WHITE);
+      LCD_ShowString(0, 0, "DANGER???", RED, WHITE);
     }
     // 평상시
     else
     {
+      LCD_Clear(WHITE);
+      LCD_ShowString(0, 0, "START!!", BLACK, WHITE);
     }
   }
   return 0;
